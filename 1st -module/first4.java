ffa671e32ffedb3e879ada428284af9cfc17dbf1
Taufiq <tm8023009@gmail.com>
2026-09-08 10:37:11 +0530

    feat: add first4 class to calculate the sum of the first 4 natural numbers

---
+++ 1st -module/first4.java
@@ -0,0 +1,8 @@
+public class first4 {
+    public static void main(String[] args) {
+        int n = 4;
+        int sum = n * (n + 1) / 2;
+
+        System.out.println("Sum of first 4 numbers = " + sum);
+    }
+}
