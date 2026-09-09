8eca525557b882a77f404ed51533cdf32a8e288e
Taufiq <tm8023009@gmail.com>
2026-09-09 10:10:04 +0530

    .

---
+++ 1st -module/Multiplication8.java
@@ -0,0 +1,9 @@
+public class Multiplication8 {
+    public static void main(String[] args) {
+        int base = 8;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(base + " x " + i + " = " + (base * i));
+        }
+    }
+}
---
+++ 1st -module/condition3.java
@@ -0,0 +1,19 @@
+public class condition3 {
+    public static void main(String[] args) {
+        int n = 21;
+
+        System.out.println("Sequence from 21:");
+
+        while (n != 1) {
+            System.out.print(n + " ");
+
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 3 + 1;
+            }
+        }
+
+        System.out.println(n);
+    }
+}
